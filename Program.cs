38d36a45c7604fcaa890e70771ef6a8b8f2bbd1f
Dawid Raj <d.raj@i4b.pl>
2023-06-04 14:16:25 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureLoginApp.Areas.Identity.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DrMarketDb") ?? throw new InvalidOperationException("Connection string 'SecureLoginAppIdentityDbContextConnection' not found.");
+
+builder.Services.AddDbContext<SecureLoginAppIdentityDbContext>(options => options.UseNpgsql(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<SecureLoginAppIdentityDbContext>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 12;
+});
+
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+    options.HttpsPort = 443;
+});
+
+builder.Services.Configure<MvcOptions>(options =>
+{
+    options.Filters.Add(new RequireHttpsAttribute());
+});
+
+builder.Services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+builder.Services.AddHsts(options =>
+{
+    options.Preload = true;
+    options.IncludeSubDomains = true;
+    options.MaxAge = TimeSpan.FromDays(60);
+});
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
